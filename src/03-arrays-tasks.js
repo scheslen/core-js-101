6c8853789bbed1cc131d5f123e8bf37a3f449dea
scheslen <antgalin@mail.ru>
2023-06-29 23:51:05 +0300

    feat: implement tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -20,8 +20,8 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -35,8 +35,13 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = [];
+  if (len > 0) {
+    arr[0] = 1;
+    for (let i = 1; i < len; i += 1) arr[i] = arr[i - 1] + 2;
+  }
+  return arr;
 }
 
 
